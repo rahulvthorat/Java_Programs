2f68785960d063d126db4f8ca0ae296f0cab9b90
pankaj <pankaj@DESKTOP-M75R6AP>
2023-03-04 16:35:45 +0530

    First commit = " Swapping Two Number "

--- Programs/src/com/dss/SwappingTwoNumber.java
+++ Programs/src/com/dss/SwappingTwoNumber.java
@@ -5,7 +5,7 @@ public class SwappingTwoNumber {
 	public static void main(String[] args) {
 		int a = 10, b = 20;
 
-		System.out.println("Before Swapping:" + a + " " + b);
+		System.out.println("Before Swapping: " + a + " " + b);
 
 		// By using third variable
 		int t = a;
@@ -16,6 +16,6 @@ public class SwappingTwoNumber {
 		/*
 		 * a=a+b; b=a-b; a=a-b;
 		 */
-		System.out.println("After Swapping:" + a + " " + b);
+		System.out.println("After Swapping: " + a + " " + b);
 	}
 }
