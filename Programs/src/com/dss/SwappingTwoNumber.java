a05c08b463d358c852720ea29a0e71bb4576f858
Rahul <Rahul>
2023-03-04 12:29:59 +0530

    First commit = Swapping Two Number

---
+++ Programs/src/com/dss/SwappingTwoNumber.java
@@ -0,0 +1,21 @@
+package com.dss;
+
+public class SwappingTwoNumber {
+
+	public static void main(String[] args) {
+		int a = 10, b = 20;
+
+		System.out.println("Before Swapping:" + a + " " + b);
+
+		// By using third variable
+		int t = a;
+		a = b;
+		b = t;
+
+		// Without using third variable
+		/*
+		 * a=a+b; b=a-b; a=a-b;
+		 */
+		System.out.println("After Swapping:" + a + " " + b);
+	}
+}
